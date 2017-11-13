49de8971c972544523f350bfa6e84cd37a3bcc43
chihaitao <cht_net@126.com>
2017-11-13 21:09:57 +0800

    Initial Commit

---
+++ HTProjectManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTProjectManager
+//
+//  Created by 池海涛 on 17/11/13.
+//  Copyright © 2017年 池海涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
